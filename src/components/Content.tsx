09ce774b321c415a6f46c183d1774b2f9ba0e4f9
Thales F Testoni <thalestestoni@gmail.com>
2022-02-16 16:31:06 -0300

    Organizating MovideCard props on Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -18,7 +18,13 @@ export function Content() {
       <main>
         <div className="movies-list">
           {movies.map(movie => (
-            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            <MovieCard 
+              key ={movie.imdbID} 
+              title={movie.Title} 
+              poster={movie.Poster} 
+              runtime={movie.Runtime} 
+              rating={movie.Ratings[0].Value} 
+            />
           ))}
         </div>
       </main>
